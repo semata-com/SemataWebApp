383332bf918e7e8d2ed9e8359ecb83674075a380
Andrew Smith <andrewedwardsmith@hotmail.co.uk>
2022-07-06 11:21:37 +0100

    Added Configuration.cs

---
+++ SemataWebApp/Shared/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SemataWebApp.Shared
+{
+    public class Configuration
+    {
+        public static HttpClient HttpClient;
+        public static string WWWRoot { get; set; }
+        public static string URL { get; set; }
+        public static string ReCaptchaSiteKey { get; set; }
+    }
+}
